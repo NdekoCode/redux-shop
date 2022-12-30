da54036de828d69f4b224a3780054d73fa3f8fb6
Ndekocode <arickbulakali@gmail.com>
2022-12-30 23:34:14 +0200

    remove: unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import { store } from "./libs/store";
-import { AppContainer } from "./views/containers";
-// import { addtoCart } from "./app/libs/actions";
-
-// Log the initial state
-console.log(store.getState());
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-//store.dispatch(addtoCart({name: "citron"}, 2))
-//store.dispatch(addtoCart({name: "kiwi"}, 5))
-unsubscribe();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppContainer />
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
