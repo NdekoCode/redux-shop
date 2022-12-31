d781f84e23ae270305053f1263ca9bba67969d33
Ndekocode <arickbulakali@gmail.com>
2022-12-31 10:19:32 +0200

    add cart component

---
+++ src/components/index.jsx
@@ -0,0 +1,32 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <span className="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  );
+};
+
+export const Card = (props) => {
+  const { item, count } = props;
+  return (
+    <div className="col-sm-4">
+      <div className="card">
+        <img width="170" height="170" src="/assets/0/citron.png" alt="Citron" />
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-6">
+              <h4>Citron</h4>
+            </div>
+            <div className="col-sm-6">
+              <p>€3/unit </p>
+              <button className="btn btn-warning btn-sm">view product</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Modal */}
+    </div>
+  );
+};
