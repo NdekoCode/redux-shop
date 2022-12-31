ab577c62585491f05b9761ac6d7c024febbb9dea
Ndekocode <arickbulakali@gmail.com>
2022-12-31 17:18:13 +0200

    add default data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from "./components/container/Card";
 import SideMenu from "./components/container/SideMenu";
 import Navbar from "./components/header/Navbar";
 
@@ -9,7 +10,16 @@ function App() {
       <div className="container">
         <div className="row">
           <SideMenu />
-          <div className="col-sm">Store</div>
+          <div className="col-sm">
+            <div className="row">
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
         </div>
       </div>
     </>
