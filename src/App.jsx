61977fc1be7bdebf43576b95c3924fbf5cd06783
Ndekocode <arickbulakali@gmail.com>
2023-01-02 22:36:00 +0200

    refactor: add pages for views layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import List from "./components/container/List";
-import SideMenu from "./components/container/SideMenu";
-import Navbar from "./components/header/Navbar";
-import { useIncrement } from "./hooks/useIncrement";
-import list from "./libs/data/data";
-
+import Home from "./components/pages/Home";
 function App() {
-  const [category, setCategory] = useState(0);
-  const [{ count }, dispatch] = useIncrement();
-  const increment = () => {
-    console.log("Increment");
-    dispatch({ type: "incr" });
-  };
-  const [search, setSearch] = useState("");
-  const [products, setProduct] = useState([]);
-  useEffect(() => {
-    setProduct(list[category]);
-    if (search.length >= 1) {
-      setProduct(
-        list
-          .flat()
-          .filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
-      );
-    }
-  }, [category, search]);
-
-  return (
-    <>
-      <Navbar search={search} setSearch={setSearch} count={count} />
-      <div className="container">
-        <div className="row">
-          <SideMenu setCategory={setCategory} category={category} />
-          <div className="col-sm">
-            <div className="row">
-              <List incrementCart={increment} data={products} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
