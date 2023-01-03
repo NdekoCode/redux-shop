342774ea433528ab49cfb97552d134bee85e1f93
Ndekocode <arickbulakali@gmail.com>
2023-01-03 15:50:52 +0200

    feat: end cartReducer

--- src/libs/store/shop/reducers.js
+++ src/libs/store/shop/reducers.js
@@ -2,17 +2,25 @@ import ACTIONS from "./actions";
 
 const { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_CART } = ACTIONS;
 const initialState = {
-  items: [],
+  cartItems: [],
 };
-const shopOnlineReducer = (state = initialState, action) => {
+export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART: {
-      return { ...state, cart: [] };
+      return { ...state, cartItems: [action.payload, ...state.items] };
     }
-    case REMOVE_FROM_CART:
-      return state;
     case UPDATE_CART:
-      return state;
+      const findAndCart = (cart) => {
+        if (cart.id === action.payload.id) {
+          cart.quantity = action.payload.quantity;
+          return cart;
+        }
+        return cart;
+      };
+      return { ...state, cartItems: state.cartItems.map(findAndCart) };
+    case REMOVE_FROM_CART:
+      const findAndDelete = (cart) => cart.id !== action.payload.id;
+      return { ...state, cartItems: state.cartItems.filter(findAndDelete) };
     default:
       return state;
   }
