9ca3d338246b67a164097e48c8978b6e53d87bda
Ndekocode <arickbulakali@gmail.com>
2023-01-03 08:13:35 +0200

    add cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Cart from "./components/pages/Cart";
 import Home from "./components/pages/Home";
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/pages/Cart.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm cart">{/* Table */}</div>
+
+          <div className="col-sm-3 order-summary">
+            <ul className="list-group">
+              <li className="list-group-item">Order Summary</li>
+
+              <li className="list-group-item">
+                <ul className="list-group flex">
+                  <li className="text-left">Subtotal</li>
+                  <li className="text-right"></li>
+                </ul>
+                <ul className="list-group flex">
+                  <li className="text-left">shipping</li>
+                  <li className="text-right">€</li>
+                </ul>
+                <ul className="list-group flex">
+                  <li className="coupon crimson">
+                    <small> Add Coupon Code</small>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="list-group-item ">
+                <ul className="list-group flex">
+                  <li className="text-left">Total</li>
+                  <li className="text-right">€</li>
+                </ul>
+              </li>
+            </ul>
+            <button
+              type="button"
+              className="btn btn-light btn-lg btn-block checkout bg-crimson"
+              disabled={true}
+            >
+              <a href="#" className="white">
+                Checkout
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cart;
