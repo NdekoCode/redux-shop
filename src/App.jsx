83d3287aa3ce560b61feb1f21fa1a081ce42f636
Ndekocode <arickbulakali@gmail.com>
2023-01-03 09:42:04 +0200

    refactor: add navbar to global pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/header/Navbar";
 import Cart from "./components/pages/Cart";
 import Home from "./components/pages/Home";
+import { useIncrement } from "./hooks/useIncrement";
+import list from "./libs/data/data";
 function App() {
+  const [category, setCategory] = useState(0);
+  const [search, setSearch] = useState("");
+  const [products, setProduct] = useState([]);
+
+  const [{ count }, dispatch] = useIncrement();
+  const increment = () => {
+    console.log("Increment");
+    dispatch({ type: "incr" });
+  };
+  useEffect(() => {
+    setProduct(list[category]);
+    if (search.length >= 1) {
+      setProduct(
+        list
+          .flat()
+          .filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
+      );
+    }
+  }, [category, search]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
+    <>
+      <Navbar search={search} setSearch={setSearch} count={count} />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              setCategory={setCategory}
+              category={category}
+              products={products}
+              increment={increment}
+            />
+          }
+        />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from "react";
-import { useIncrement } from "../../hooks/useIncrement";
-import list from "../../libs/data/data";
 import List from "../container/List";
 import SideMenu from "../container/SideMenu";
-import Navbar from "../header/Navbar";
 
-function Home() {
-  const [category, setCategory] = useState(0);
-  const [{ count }, dispatch] = useIncrement();
-  const increment = () => {
-    console.log("Increment");
-    dispatch({ type: "incr" });
-  };
-  const [search, setSearch] = useState("");
-  const [products, setProduct] = useState([]);
-  useEffect(() => {
-    setProduct(list[category]);
-    if (search.length >= 1) {
-      setProduct(
-        list
-          .flat()
-          .filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
-      );
-    }
-  }, [category, search]);
-
+function Home({ setCategory, products, category, increment }) {
   return (
     <>
-      <Navbar search={search} setSearch={setSearch} count={count} />
       <div className="container">
         <div className="row">
           <SideMenu setCategory={setCategory} category={category} />
