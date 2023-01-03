fda06d91206e0295a87490ac68b3655a154bcaca
Ndekocode <arickbulakali@gmail.com>
2023-01-03 15:12:04 +0200

    feat: init redux by adding cart actions definitions

---
+++ src/libs/store/shop/actions.js
@@ -0,0 +1,9 @@
+/**
+ * definitions des actions
+ */
+const ACTIONS = {
+  ADD_TO_CART: "ADD_TO_CART",
+  UPDATE_CART: "UPDATE_CART",
+  REMOVE_FROM_CART: "REMOVE_FROM_CART",
+};
+export default ACTIONS;
