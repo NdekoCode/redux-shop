f60050c809ddb96de8352b1b8af9ae511957fff9
Ndekocode <arickbulakali@gmail.com>
2023-01-03 15:12:46 +0200

    feat: addToCart action for adding a product to a cart

--- src/libs/store/shop/actions.js
+++ src/libs/store/shop/actions.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from "uuid";
 /**
  * definitions des actions
  */
@@ -5,5 +6,16 @@ const ACTIONS = {
   ADD_TO_CART: "ADD_TO_CART",
   UPDATE_CART: "UPDATE_CART",
   REMOVE_FROM_CART: "REMOVE_FROM_CART",
+};
+const { ADD_TO_CART } = ACTIONS;
+export const addToCart = (item) => {
+  return {
+    type: ADD_TO_CART,
+    payload: {
+      id: uuid(),
+      qty: 1,
+      ...item,
+    },
+  };
 };
 export default ACTIONS;
