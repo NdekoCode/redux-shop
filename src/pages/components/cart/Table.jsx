b6afeab8a7449bae749babfd3fdf3483ec64ed7c
Ndekocode <arickbulakali@gmail.com>
2023-01-05 19:31:08 +0200

    feat: update a product quantity

--- src/pages/components/cart/Table.jsx
+++ src/pages/components/cart/Table.jsx
@@ -1,4 +1,19 @@
+import { useDispatch } from "react-redux";
+import { useIncrement } from "../../../libs/hooks/useIncrement";
+import { updateCart } from "../../../libs/store/shop/actions";
+
 export const Row = ({ item }) => {
+  const dispatch = useDispatch();
+  const [{ count }, dispatchCount] = useIncrement(item.quantity);
+  const increment = () => {
+    dispatchCount({ type: "incr" });
+    update(item, count);
+  };
+  const decrement = () => {
+    dispatchCount({ type: "decr" });
+    update(item, count);
+  };
+  const update = (item, count) => dispatch(updateCart(item, count));
   return (
     <tr>
       <td>
@@ -7,30 +22,31 @@ export const Row = ({ item }) => {
           height="70"
           src={`/assets/${item.category}/${item.image}`}
           alt={item.name}
+          title={item.name}
         />
       </td>
-      <td>{item.name}</td>
+      <td>{item.ref}</td>
       <td>€{item.price}</td>
       <td>
         <div className="btn-group" role="group" aria-label="Basic example">
           <button
             type="button"
             className="btn btn-secondary"
-            onClick={() => {}}
+            onClick={decrement}
           >
             -
           </button>
-          <span className="btn btn-light">{item.quantity}</span>
+          <span className="btn btn-light">{count}</span>
           <button
             type="button"
             className="btn btn-secondary"
-            onClick={() => {}}
+            onClick={increment}
           >
             +
           </button>
         </div>
       </td>
-      <td>€{(item.price * item.quantity).toFixed(2)}</td>
+      <td>€{(item.price * count).toFixed(2)}</td>
       <td>
         <button
           type="button"
