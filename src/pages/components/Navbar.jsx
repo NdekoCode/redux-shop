151e2c23e5f54beb7e8ae819c168e2371fcbee62
Ndekocode <arickbulakali@gmail.com>
2023-01-06 12:02:13 +0200

    refactor: navbar links

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -15,52 +15,62 @@ export const Navbar = ({ search, setSearch }) => {
     }
   };
   return (
-    <nav className="navbar orange navbar-expand-lg navbar-light bg-light fixed-top">
-      <Link className="navbar-brand crimson" to="/">
-        <i className="fas fa-shopping-cart"></i> Mes Courses en Ligne
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <>
+      <nav className="navbar orange navbar-expand-lg bg-light  fixed-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand crimson" to="/">
+            <i className="fas fa-shopping-cart"></i> Mes Courses en Ligne
+          </Link>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <div className="ms-auto cart">
-          <div>
-            <form
-              className="search form-inline my-2 my-lg-0"
-              onSubmit={handleSubmit}
-            >
-              <input
-                className="form-control mr-sm-2"
-                type="search"
-                value={search}
-                onChange={chandleSearch}
-                name="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-            </form>
-          </div>
-          <div className="menu-right ms-auto">
-            {/* Cart */}
-            <Link to="/cart">
-              <i className="fas fa-shopping-bag fa-2x grey"></i>
-            </Link>
-            <span className="badge top-0 p-1 bg-success rounded-circle">
-              {cartItems.length > 0 && cartItems.length}
-            </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="ms-auto cart">
+              <form
+                role="search"
+                className="search d-flex my-2 my-lg-0"
+                onSubmit={handleSubmit}
+              >
+                <input
+                  className="form-control mr-sm-2 rounded-0 rounded-start"
+                  type="search"
+                  value={search}
+                  onChange={chandleSearch}
+                  name="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button
+                  className="btn btn-outline-success rounded-0 rounded-end"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+              <div className="navbar-nav menu-right ms-auto mb-lg-0">
+                {/* Cart */}
+                <Link to="/cart" className=" ms-3">
+                  <i className="fas fa-shopping-bag fa-2x grey"></i>
+                </Link>
+                <span className="badge top-0 p-1 bg-success rounded-circle">
+                  {cartItems.length > 0 && cartItems.length}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 export default Navbar;
